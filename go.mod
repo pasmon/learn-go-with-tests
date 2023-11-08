2ba480c781c5f0ee219f0d2aa3b1274a655a249e
pasmon <disssko@gmail.com>
2023-11-08 10:25:27 +0200

    feat: code and tests in multiple languages

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/juupas/learn-go-with-tests
+
+go 1.21.1
---
+++ hello_test.go
@@ -0,0 +1,38 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Pasi", "")
+		want := "Hello, Pasi"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Dominique", "French")
+		want := "Bonjour, Dominique"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
